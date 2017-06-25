812c056a06193938b68dcc60d5680d3049a57efb
negebauer <negebauer@uc.cl>
2017-06-25 18:02:07 -0400

    Update run script

--- run.sh
+++ run.sh
@@ -1,39 +1,26 @@
 # Clear previous compiled
-rm secuential
-rm parallel
+rm t2.o
 
 # Compile
 make
 
 # Image and param to use
-img_def='big'
-kernel_def='blur_3'
-repeat_def='3'
-if [[ -z $1 ]]; then img_path="test/img/${img_def}.png"; else img_path=$1; fi
-if [[ -z $2 ]]; then kernel_path="test/kernel/${kernel_def}.txt"; else kernel_path=$2; fi
-if [[ -z $3 ]]; then repeat=$repeat_def; else repeat=$3; fi
+test_def='t2'
+if [[ -z $1 ]]; then test_path="test/${img_def}.txt"; else test_path=$1; fi
 
-img_file="${img_path##*/}"
-img_name="${img_file%.*}"
-kernel_file="${kernel_path##*/}"
-kernel_name="${kernel_file%.*}"
-img_out_path="test/out/${img_name}_${kernel_name}_${repeat}.png"
-
-params="$img_path $kernel_path $img_out_path $repeat"
-
-echo "
-Apply mask $kernel_name to $img_name $repeat times
-"
+test_file="${test_path##*/}"
+test_name="${test_file%.*}"
+test_out="testout/${test_name}"
 
 # Run stuff
 print() {
-  echo "--- $1 ---"
+  echo "--- $test_name ---"
 }
-print 'parallel'
-time ./parallel $params
-mv $img_out_path i_parallel.png
-echo ''
-print 'secuential'
-time ./secuential $params
-mv $img_out_path i_secuential.png
-cp i_parallel.png $img_out_path
+# print 'parallel'
+time ./t2.o $test_name
+# mv $img_out_path i_parallel.png
+# echo ''
+# print 'secuential'
+# time ./secuential $params
+# mv $img_out_path i_secuential.png
+# cp i_parallel.png $img_out_path
