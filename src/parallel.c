9376c614b6a184b52c9b456aac2fbfcf9066e4be
negebauer <negebauer@uc.cl>
2017-06-26 01:07:28 -0400

    Comment stuff and example

--- src/parallel.c
+++ src/parallel.c
@@ -42,27 +42,33 @@ int main(int argc, char *argv[]) {
     return 1;
   }
   char *input = argv[1];
-  WSP *wsp = wspInit(input);
-  Route *route = routeInit(wsp);
+  // WSP *wsp = wspInit(input);
+  // Route *route = routeInit(wsp);
 
-  int size, rank, len, rc;
-  char hostname[MPI_MAX_PROCESSOR_NAME];
-  rc = MPI_Init(&argc, &argv);
-  if (rc != MPI_SUCCESS) {
-    printf("Error starting MPI program. Terminating.\n");
-    MPI_Abort(MPI_COMM_WORLD, rc);
-  }
+  int size, rank, dest, source, rc, count, tag = 1;
+  char inmsg, outmsg = "x";
+  MPI_Status Stat;
+  MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Get_processor_name(hostname, &len);
+  if (rank == 0) {
+    dest = 1;
+    source = 1;
+    rc = MPI_Send(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
+    rc = MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
+  } else if (rank == 1) {
+    dest = 0;
+    source = 0;
+    rc = MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
+    rc = MPI_Send(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
+  }
+  rc = MPI_Get_count(&Stat, MPI_CHAR, &count);
+  printf("Task %d: Received %d char(s) from task %d with tag %d \n", rank,
+         count, Stat.MPI_SOURCE, Stat.MPI_TAG);
 
-  printf("Number of tasks= %d My rank= %d Running on %s\n", size, rank,
-         hostname);
-  /******* do some work *******/
-  dfs(wsp, route);
   MPI_Finalize();
 
-  wspPrintRoute(wsp);
-  wspFree(wsp);
+  // wspPrintRoute(wsp);
+  // wspFree(wsp);
   return 0;
 }
