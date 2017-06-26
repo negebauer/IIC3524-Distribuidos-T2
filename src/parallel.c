32b4ffc558e4578d3c3dea4fbc67bfdab2e83df7
negebauer <negebauer@uc.cl>
2017-06-26 01:04:57 -0400

    Starting to shape parallel i gues

--- src/parallel.c
+++ src/parallel.c
@@ -4,6 +4,36 @@
 #include <stdlib.h>
 #include <time.h>
 
+void dfs(WSP *wsp, Route *route) {
+  // If route completed, check if best
+  if (route->cities[wsp->size - 1] != 0) {
+    if (wsp->cost == -1 || route->cost < wsp->cost) {
+      wsp->cost = route->cost;
+      for (int i = 0; i < wsp->size; i++) {
+        wsp->cities[i] = route->cities[i];
+      }
+    }
+    return;
+  }
+
+  // If worse than best, stop travelling
+  if (wsp->cost != -1 && route->cost >= wsp->cost) {
+    // printf("Worse\t");
+    // routePrint(route);
+    return;
+  }
+
+  // Keep travelling
+  for (int destination = 1; destination < wsp->size; destination++) {
+    // printf("Checking destination %i\n", destination);
+    if (routeShouldVisit(route, destination)) {
+      routeAdvance(wsp, route, destination);
+      dfs(wsp, route);
+      routeReturn(wsp, route, destination);
+    }
+  }
+}
+
 int main(int argc, char *argv[]) {
   /* The program receives 1 param */
   if (argc != 2) {
@@ -13,6 +43,7 @@ int main(int argc, char *argv[]) {
   }
   char *input = argv[1];
   WSP *wsp = wspInit(input);
+  Route *route = routeInit(wsp);
 
   int size, rank, len, rc;
   char hostname[MPI_MAX_PROCESSOR_NAME];
@@ -24,11 +55,14 @@ int main(int argc, char *argv[]) {
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Get_processor_name(hostname, &len);
-  printf("Number of tasks= %d My rank= %d Running on %s\n", size, rank,
-         hostname); /******* do some work *******/
-  MPI_Finalize();
-  // Solve
 
+  printf("Number of tasks= %d My rank= %d Running on %s\n", size, rank,
+         hostname);
+  /******* do some work *******/
+  dfs(wsp, route);
+  MPI_Finalize();
+
+  wspPrintRoute(wsp);
   wspFree(wsp);
   return 0;
 }
