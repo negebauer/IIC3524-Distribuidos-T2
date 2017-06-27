157871686d8fa5ccb94e59c9f1b8b839536567b0
negebauer <negebauer@uc.cl>
2017-06-27 00:47:17 -0400

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 folder='T2'
 cd -- "$(dirname "$BASH_SOURCE")"
-rsync -e "ssh -i ~/.ssh/hercules" -azP --exclude-from 'exclude.txt' --delete ../$folder/ negebauer@hercules.ing.puc.cl:$folder
+rsync -e "ssh -i ~/.ssh/hercules" -azP --exclude-from 'exclude.txt' --delete-after ../$folder/ negebauer@hercules.ing.puc.cl:$folder
